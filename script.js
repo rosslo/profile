709013c2863adb60443b154db0de6e5ca25c50d8
rosslo <wewe381h@gmail.com>
2017-01-06 18:25:22 +0800

    delete img

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
 var startY = $('#part-1').offset().top; /*#part-1  .rocket-body-2'*/
-//var startY = $('.rocket-top').offset().top;
 $('html, body').animate({scrollTop: startY},1);
 wheelRight();
 var opendoor=false; //用來判斷是否以滾動過執行opendoor();
@@ -52,6 +51,10 @@ function wheelRight(){
 			$('body').animate({scrollTop: Y}, 800, function(){
 				animated=true;
 				infoMove();
+				if(Y===0){
+					$('.rocket').addClass('rocket-move');
+					$('.contact-item, .contact-title').css("opacity","1");
+				}
 			});
 		}
       }
@@ -65,11 +68,15 @@ function openDoor(){
 	var part1 = document.getElementById('part-1');
 	var part2 = document.getElementById('part-2');
 	$('.entrance').css("transform","scale(1.5)");
+	$('.tip').css("display","none");
 	door[0].style.top="-80%";
 	door[1].style.top="70%";
 	part2.style.opacity="1";
 	setTimeout(function(){
 		$('.entrance').css("display","none");
+		 $('.box').addClass('box-remove');
+	}, 3000);
+	setTimeout(function(){
 		opendoor=true;
 	}, 4000);
 }
@@ -81,7 +88,7 @@ function showSkills(){
 	$('.skill-level').each(function(){
 		var level = $(this).attr('data-level');
 		$(this).animate({width:level+"%"},800);
-		$(this).css("padding","3px 5px");
+		$(this).css("padding-left","5px");
 	});
 }
 });
