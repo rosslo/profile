e50b51fa9b253020ea1a3b994b61bc5c85981e32
rosslo <wewe381h@gmail.com>
2017-01-09 23:26:03 +0800

    add keyboard event & translate3D

--- script.js
+++ script.js
@@ -7,14 +7,17 @@ var checkCssLoad = setInterval(function(){
 		});
 	}
 },50);
-var startY = $(window).height()*4; /*#part-1  .rocket-body-2'*/
-$('html, body').animate({scrollTop: startY},1);
+//var startY = $(window).height()*4; /*#part-1  .rocket-body-2'*/
+// $('html, body').animate({scrollTop: startY},1);
 var opendoor=false; //用來判斷是否以滾動過執行opendoor();
 var animated = true; //用來避免animate執行scroll的延遲
 var scrollRight = true; //scrollRight=true=>right, false=>down;
 var ismobile = detectmob();
+var vhFixed;
+var translateSpeed = 0; //全屏滾動速度
 if(ismobile){
-	var vhFixed = vhFix();
+	translateSpeed = 800;
+	vhFixed = vhFix();
 	$('.tip.-web').hide();
 	$("body, .work, .work-link").swipe( {
         //Generic swipe handler for all directions
@@ -30,60 +33,81 @@ if(ismobile){
 	    threshold:1
       });
 }else{
-	var vhFixed = true;
+	translateSpeed = 1200;
+	vhFixed = true;
+	var translateDone = true;
 	$('.tip.-mobile').hide();
+	$(window).keydown(function(event){
+		switch(event.keyCode) {
+		  case 38:
+		   wheelRight(1);
+		  break;
+		  case 40:
+		   wheelRight(-1);
+		  break;
+		}
+	});
 	$("body").mousewheel(function(event, delta) {
-		wheelRight(delta);
+		if(translateDone){
+			wheelRight(delta);
+			translateDone = false;
+		}
 		event.preventDefault();
+	    setTimeout(function(){
+  			translateDone = true;
+		}, 1200);
 	    return false;
 	});
 }
 function wheelRight(delta){
-     //this.scrollLeft -= (delta * 50);
     if(loaded&&vhFixed){
      if(!opendoor){openDoor();}
      else{
-	    var horizontalNum = $(".horizontal>li").length; //水平的part數量
-	    var vw100=$('body').width(); //100vw的值多少
-	    var cur = parseInt($('.horizontal').css('transform').split(',')[4]);
-	    var X=cur; //X軸將移動距離的儲存變數，初始為現在位置
-	    if(Math.abs(cur)%vw100===0 && scrollRight){ //判斷是否整個頁面滑動結束，避免連續滑動
-		    if(delta===-1 && (Math.abs(cur)/vw100<horizontalNum-1)){ //-1往下一頁 && 判斷時候是否最後一個part
-		      	X=cur-vw100;
-		      	if(Math.abs(cur)/vw100===horizontalNum-3){
+	    var HendIndex = 2;//$(".horizontal>li").length-2 //水平part最後一個index，扣除entrance以及長度多1
+	    var translateX = parseInt($('.horizontal').css('transform').split(',')[4]) || parseInt($('.horizontal').css('-webkit-transform').split(',')[4]);
+	    var HcurIndex = Math.floor(Math.abs(translateX)/($('body').width()-1)); //水平part現在的index
+	    var VendIndex = 3; //$(".vertical>li").length-1; //水平part最後一個index，扣除長度多1
+	    var translateY = parseInt($('.vertical').css('transform').split(',')[5]) || parseInt($('.vertical').css('-webkit-transform').split(',')[5]);
+	    var VcurIndex = 4-Math.floor((Math.abs(translateY)/$(window).height())); //水平part現在的index
+	    if(scrollRight){ //判斷是否整個頁面滑動結束，避免連續滑動
+		    if(delta===-1 && HcurIndex!==HendIndex){ //-1往下一頁 && 判斷時候是否最後一個part
+		      	HcurIndex++;
+		      	if(HcurIndex===1){
 		      		setTimeout(function(){
 		      			showSkills();
-				}, 1200);
+					}, translateSpeed);
 		      	}
-		      	if(Math.abs(cur)/vw100===horizontalNum-2){
+		      	if(HcurIndex===2){
 		      		setTimeout(function(){
-		      			infoMove();
-				}, 1200);
+		      			infoMove(VcurIndex);
+					}, translateSpeed);
 		      	}
-		    }else if(delta===1 && cur<0){ //1往上一頁 && cur===0時，代表在第一個part
-		      	X=cur+vw100;
-		    }else if(delta===-1 && (Math.abs(cur)/vw100===horizontalNum-1)){  //-1往下一頁 && 已經在水平part的最後一個
+		      	translate3d('.horizontal',"-"+(HcurIndex*33.33).toString()+"%","0","0");
+		    }else if(delta===1 && HcurIndex>0){ //1往上一頁 && cur===0時，代表在第一個part
+		      	HcurIndex--;
+		        translate3d('.horizontal',"-"+(HcurIndex*33.33).toString()+"%","0","0");
+		    }else if(delta===-1 && (HcurIndex===HendIndex)){  //-1往下一頁 && 已經在水平part的最後一個
 		    	scrollRight=false;
 		    }
-		    $('.horizontal').css('transform','translateX('+X+'px)');
 	    }
-		var vh100=$(window).height(); //100vh的值多少
-		var Y = $(window).scrollTop(); //Y軸將移動距離的儲存變數，初始為現在位置
-		if(!scrollRight&&animated){
-			if(delta===-1){
-				Y-=vh100;
-			}else if(delta===1 && Y!=startY){  //1往上一頁 && 不處於垂直part的最下面一個(載入起始位置startY)
-				Y+=vh100;
-			}else if(delta===1 && (Math.abs(cur)/vw100===horizontalNum-1)){  //1往上一頁 && 已經在水平part的最後一個
+		if(!scrollRight){
+			if(delta===-1 && VcurIndex<4){
+				VcurIndex++;
+				translate3d('.horizontal',translateX+"px",(VcurIndex*100).toString()+"%","0");
+			}else if(delta===1 && VcurIndex>0){  //1往上一頁 && 不處於垂直part的最下面一個(載入起始位置startY)
+				VcurIndex--;
+				translate3d('.horizontal',translateX+"px",(VcurIndex*100).toString()+"%","0");
+			}else if(delta===1 && VcurIndex===0){  //1往上一頁 && 已經在水平part的最後一個
 		    	scrollRight=true;
-		    	X=cur+vw100;
-		    	$('.horizontal').css('transform','translateX('+X+'px)');
+		    	translate3d('.horizontal',"-"+((HcurIndex-1)*33.33).toString()+"%","0","0");
 		    }
-			animated=false;
-			$('html,body').animate({scrollTop: Y}, 800, function(){
-				animated=true;
-				infoMove();
-				if(Y===0){
+		    console.log(VcurIndex);
+			var Y = ((4-VcurIndex)*25).toString();
+			translate3d('.vertical',"0","-"+Y+"%","0");
+			setTimeout(function(){
+		      	infoMove(4-VcurIndex);
+			}, translateSpeed);
+				if(VcurIndex===4){
 					if(ismobile){
 						$('.rocket').css({"left":($('body').width()/20).toString()+'px',"bottom":($('body').width()/10*3).toString()+'px',"transform":"rotate(45deg) scale(1)","-webkit-transform":"rotate(45deg) scale(1)","-moz-transform":"rotate(45deg) scale(1)","-o-transform":"rotate(45deg) scale(1)"});
 					}else{
@@ -91,10 +115,13 @@ function wheelRight(delta){
 					}
 					$('.contact-item, .contact-title').css("opacity","1");
 				}
-			});
 		}
       }
       }
+}
+function translate3d(dom,x,y,z){
+	$(dom).css('transform',"translate3d("+x+","+y+","+z+")");
+	$(dom).css('-webkit-transform',"translate3d("+x+","+y+","+z+")");
 }
 function openDoor(){
 	var border = document.getElementsByClassName('border');
@@ -114,20 +141,8 @@ function openDoor(){
 		opendoor=true;
 	}, 4000);
 }
-function infoMove(){
-	var index = $(window).scrollTop()/$(window).height()-1;
-	if(window.screen.availHeight>window.screen.availWidth){
-		if(index===3){
-			$('.info-box').eq(3).addClass('info-less-move');
-		}else if(index===1){
-			$('.info-box').eq(1).addClass('info-less-move');
-		}
-		else{
-			$('.info-box').eq(index).addClass('info-move');
-		}
-	}else{
-		$('.info-box').eq(index).addClass('info-move');
-	}
+function infoMove(Vindex){
+	$('.info-box').eq(Vindex).addClass('info-move');
 }
 function showSkills(){
 	// if(window.screen.availHeight<window.screen.availWidth){
